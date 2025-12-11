6535dd3811b77a5c057da2d733513d99f53d6a63
default <isaac.biggs@stu.ocsb.ca>
2025-12-11 14:37:10 +0000

    Worked on program to get average of marks

---
+++ main.ts
@@ -0,0 +1,21 @@
+/**
+ * @author Isaac Biggs
+ * @version 1.0.0
+ * @date 2025-12-10
+ * @fileoverview This program will calculate the average of a set of provided marks
+*/
+//variables 
+let numbermarks: string;
+let amountmarks: number;
+let averagegrade = "";
+//prompt user for number of marks
+numbermarks = prompt("How many marks will you enter today?") || "No marks entered!";
+
+//covert string to numbner
+amountmarks = parseInt(numbermarks);
+
+//loop through the number of marks
+for (let average = 1; average <= amountmarks; average++);
+averagegrade +=(average)
+
+console.log(averagegrade)
